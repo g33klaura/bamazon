6531d575983f5fde3803b06de25bb095eaae8586
Laura Boetcher <g33klaura@gmail.com>
2017-10-20 20:32:03 -0400

    Add npm package requirements

---
+++ bamCRUD.js
@@ -0,0 +1,26 @@
+// JavaScript for Bamazon
+// Steps to complete:
+// []
+
+
+
+// REQUIREMENTS ========================================
+// 
+
+const mysql = require('mysql');
+const table = require('cli-table');
+
+
+
+// VARIABLES & CONSTRUCTORS ============================
+// 
+
+
+
+// FUNCTIONS ===========================================
+//
+
+
+
+// MAIN PROCESS ========================================
+// 
