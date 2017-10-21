d6d19180822f6494041ebff503cc2ba4d74a24cb
Laura Boetcher <g33klaura@gmail.com>
2017-10-20 21:26:58 -0400

    Add cli-table req

---
+++ bamazonCustomer.js
@@ -0,0 +1,23 @@
+// REQUIREMENTS ========================================
+// 
+
+// const mysql = require('mysql');
+const table = require('cli-table');
+
+const bamCRUD = require('./bamCRUD.js');
+
+
+
+
+// VARIABLES & CONSTRUCTORS ============================
+// 
+
+
+
+// FUNCTIONS ===========================================
+//
+
+
+
+// MAIN PROCESS ========================================
+// 
