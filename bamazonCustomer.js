d79d2a662da45ef16eab87912776e08072e9e0f9
Laura Boetcher <g33klaura@gmail.com>
2017-10-20 21:46:57 -0400

    Quick tryout cli-table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,7 @@
 // 
 
 // const mysql = require('mysql');
-const table = require('cli-table');
+const Table = require('cli-table');
 
 const bamCRUD = require('./bamCRUD.js');
 
@@ -11,6 +11,19 @@ const bamCRUD = require('./bamCRUD.js');
 
 // VARIABLES & CONSTRUCTORS ============================
 // 
+
+let table = new Table({
+	head: ['TH 1 id', 'TH 2 product_name'],
+	colWidths: [5, 60]
+});
+
+// So... how to get the values to be read from the database....
+// let query = connection.query ....
+// but that's in 'bamCRUD.js'....
+// table.push(
+// 	['First value', 'Second value'],
+// );
+console.log(table.toString());
 
 
 
