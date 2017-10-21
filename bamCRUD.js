4f79c08fdef207a9a7e0ec483947e3734d02721f
Laura Boetcher <g33klaura@gmail.com>
2017-10-20 21:26:36 -0400

    Add connection code

--- bamCRUD.js
+++ bamCRUD.js
@@ -1,7 +1,13 @@
 // JavaScript for Bamazon
-// Steps to complete:
-// []
 
+/*
+Steps to complete:
+[] Connection to database
+[] 'bamazonCustomer.js' displays all items for sale
+
+Q: WHICH FILE LINKS TO WHICH???
+If bamazonCustomer runs, THAT needs the 'cli-table' req
+*/
 
 
 // REQUIREMENTS ========================================
@@ -14,6 +20,22 @@ const table = require('cli-table');
 
 // VARIABLES & CONSTRUCTORS ============================
 // 
+
+const connection = mysql.createConnection({
+	host: 127.0.0.1,
+	port: 3306,
+	user: 'root',
+	password: ''.
+	database: 'bamazon'
+});
+connection.connect(function(error) {
+	if (error) throw error;
+	console.log('connected as id ' + connection.threadId + '\n');
+
+	// createProduct(); <~from iceCreamCRUD activity
+});
+
+
 
 
 
