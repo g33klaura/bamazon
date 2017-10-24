fd0d2d8ae30ca2c30a828ba9e9907cade68a165b
Laura Boetcher <g33klaura@gmail.com>
2017-10-23 22:58:59 -0400

    Comment-out everything

--- bamCRUD.js
+++ bamCRUD.js
@@ -1,34 +1,36 @@
 // Nevermind, forgot this controls the package.json...
+// Does it matter if there's anything actually on here or not tho...
 
 
 // REQUIREMENTS ========================================
 // 
 
-const mysql = require('mysql');
-const table = require('cli-table');
+const Table = require('cli-table');
 const colors = require('colors');
+const inquirer = require('inquirer');
+
+const mysql = require('mysql');
+// const connection = mysql.createConnection({
+// 	host: '127.0.0.1',
+// 	port: 3306,
+// 	user: 'root',
+// 	password: '',
+// 	database: 'bamazon'
+// });
+
+// connection.connect(function(error) {
+// 	if (error) throw error;
+// 	console.log('connected as id ' + connection.threadId + '\n');
+
+// 	displayProducts();
+// });
+
+// module.exports = connection;
 
 
 
 // VARIABLES & CONSTRUCTORS ============================
 // 
-
-const connection = mysql.createConnection({
-	host: '127.0.0.1',
-	port: 3306,
-	user: 'root',
-	password: '',
-	database: 'bamazon'
-});
-
-connection.connect(function(error) {
-	if (error) throw error;
-	console.log('connected as id ' + connection.threadId + '\n');
-
-	// createProduct(); <~from iceCreamCRUD activity
-});
-
-
 
 
 
@@ -39,3 +41,5 @@ connection.connect(function(error) {
 
 // MAIN PROCESS ========================================
 // 
+
+// If get to multiple logins part, should have the switch statement to handle which command is entered on here?
