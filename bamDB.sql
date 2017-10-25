ec7b40a38f97ce068fd6a22cc682208c9916906f
Laura Boetcher <g33klaura@gmail.com>
2017-10-25 01:27:19 -0400

    Update database to include product type in name

--- bamDB.sql
+++ bamDB.sql
@@ -18,42 +18,42 @@ CREATE TABLE products (
 
 -- Item 1
 INSERT INTO products (product_name, dept_name, price, on_hand_qty)
-VALUES ('Professor Layton and the Azran Legacy', 'Video Game', 54.28, 1);
+VALUES ('Professor Layton and the Azran Legacy 3DS Games', 'Video Game', 54.28, 4);
 
 -- Item 2
 INSERT INTO products (product_name, dept_name, price, on_hand_qty)
-VALUES ('Mario and Luigi: Dream Team', 'Video Game', 34.99, 4);
+VALUES ('Mario and Luigi: Dream Team 3DS Games', 'Video Game', 34.99, 8);
 
 -- Item 3
 INSERT INTO products (product_name, dept_name, price, on_hand_qty)
-VALUES ('Ready Player One', 'Book', 9.99, 118);
+VALUES ('Ready Player One by Ernest Cline Books', 'Book', 9.99, 118);
 
 -- Item 4
 INSERT INTO products (product_name, dept_name, price, on_hand_qty)
-VALUES ('Dark Crystal, The / Labyrinth / Mirrormask', 'DVD', 9.44, 14);
+VALUES ('Dark Crystal, The / Labyrinth / Mirrormask DVDs', 'DVD', 9.44, 14);
 
 -- Item 5
 INSERT INTO products (product_name, dept_name, price, on_hand_qty)
-VALUES ('Broad City, Season 1', 'DVD', 9.96, 22);
+VALUES ('Broad City, Season 1 DVDs', 'DVD', 9.96, 22);
 
 -- Item 6
 INSERT INTO products (product_name, dept_name, price, on_hand_qty)
-VALUES ('Armada', 'Book', 16.00, 42);
+VALUES ('Armada by Ernest Cline Books', 'Book', 15.99, 42);
 
 -- Item 7
 INSERT INTO products (product_name, dept_name, price, on_hand_qty)
-VALUES ('There\'s a Hair in My Dirt!', 'Book', 10.62, 162);
+VALUES ('There\'s a Hair in My Dirt! by Gary Larson Books', 'Book', 10.62, 162);
 
 -- Item 8
 INSERT INTO products (product_name, dept_name, price, on_hand_qty)
-VALUES ('Exploding Kittens Card Game', 'Game', 19.99, 2);
+VALUES ('Exploding Kittens Card Games', 'Game', 19.99, 88);
 
 -- Item 9
 INSERT INTO products (product_name, dept_name, price, on_hand_qty)
-VALUES ('Magnetic Travel Chess', 'Game', 10.96, 15);
+VALUES ('Magnetic Travel Chess Games', 'Game', 10.96, 15);
 
 -- Item 10
 INSERT INTO products (product_name, dept_name, price, on_hand_qty)
-VALUES ('Lovely: Ladies of Animation', 'Book', 23.91, 9);
+VALUES ('Secret Aardvark Habanero Hot Sauce', 'Food', 9.99, 9);
 
 -- ~WORKS!
